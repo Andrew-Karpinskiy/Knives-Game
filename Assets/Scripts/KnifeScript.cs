81fe747f568e7848fdf8ea51b62d9bda56ad764b
andrew_karpinskiy <pnc7223@gmail.com>
2020-01-15 16:05:17 +0200

    Добавлена возможность вращения мишени с рандомной скоростью и направлением

--- Assets/Scripts/KnifeScript.cs
+++ Assets/Scripts/KnifeScript.cs
@@ -6,55 +6,33 @@ public class KnifeScript : MonoBehaviour
 {
     [SerializeField]
     private Vector2 throwForce;
-
     private Rigidbody2D rb;
-
-    private bool isActive = true;
-
-    private int availableKnives;
+    private bool isKnifeActive = true;
 
     private void Awake()  {
-        rb = GetComponent<Rigidbody2D>();
-        availableKnives = SetAvailableKnives();
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     // Start is called before the first frame update
     void Start() {
-        
     }
 
     // Update is called once per frame
     void Update() {
-
-        if (Input.GetMouseButtonDown(0) && isActive && availableKnives > 0 ) {
+        if (Input.GetMouseButtonDown(0) && isKnifeActive) {
             rb.AddForce(throwForce, ForceMode2D.Impulse);
             rb.gravityScale = 1;
         }
     }
 
-    private int SetAvailableKnives () {
-        //Проверить нормальную работу рандома(диапазон)
-        return Random.Range( 4, 8 );
-    }
-
     void OnCollisionEnter2D(Collision2D collision) {
-
-        if(!isActive)
+        if(!isKnifeActive)
             return;
-       
-        isActive = false;
-        availableKnives--;
-
-
+        isKnifeActive = false;
         if (collision.collider.tag == "Target") {
-
             rb.velocity = new Vector2(0, 0);
             rb.bodyType = RigidbodyType2D.Kinematic;
             this.transform.SetParent(collision.collider.transform);
-
         }
-
-
     }
-
 }
---
+++ Assets/Scripts/MotorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorController : MonoBehaviour
+{
+    private WheelJoint2D wheelJoint;
+    private JointMotor2D jointMotor;
+    public float[] rotationSpeed = new float [] {100,-100,200,-300,350};
+    public float[] rotationTime = new float [] {1f,0.5f,2f,3f,2.5f,1.5f};
+
+
+    private void Awake()  {
+        wheelJoint = GetComponent<WheelJoint2D>();
+        jointMotor = new JointMotor2D();    
+        StartCoroutine("RotateController");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //    // jointMotor.motorSpeed = speeds[Random.Range(0, speeds.Length)];
+    // }
+
+    IEnumerator RotateController() {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            jointMotor.motorSpeed = rotationSpeed[Random.Range(0, rotationSpeed.Length)];
+            jointMotor.maxMotorTorque = 10000;
+            wheelJoint.motor = jointMotor;
+            yield return new WaitForSeconds(rotationTime[Random.Range(0, rotationTime.Length)]);
+        }
+
+    }
+
+}
